49d03ff9e983d9050e3b46702408c1e631f0f456
vishvajeet dubey <64639869+vishvajeet-dubey@users.noreply.github.com>
2020-11-05 01:04:38 +0530

    Update java_6_Java_operator.java

--- java_6_Java_operator.java
+++ java_6_Java_operator.java
@@ -11,22 +11,22 @@ package learning;
 	- 5. Bitwise operators
 	
 	1. ARIRHMETIC OPERATORS
-	--------------------------------------------------------------------------------------
-    OPERATOR |   NAME			|       DISCRIPTION 						|  EXAMPLE   |
-    --------------------------------------------------------------------------------------
-		+	 |	Addition		|	Adds together two values				|	x + y    |	
+	------------------------------------------------------------------------------------------------------------------------------------
+    	   OPERATOR 	 |   NAME			|       DISCRIPTION 					|  EXAMPLE   |
+    ----------------------------------------------------------------------------------------------------------------------------------------
+		+	 |	Addition		|	Adds together two values			|	x + y    |	
 		-	 |	Subtraction		|	Subtracts one value from another		|	x - y    |	
- 		*	 |	Multiplication	|	Multiplies two values					|	x * y    |	
+ 		*	 |	Multiplication		|	Multiplies two values				|	x * y    |	
 		/	 |	Division		|	Divides one value by another			|	x / y    |	
 		%	 |	Modulus			|	Returns the division remainder			|	x % y    |	
-		++	 |	Increment		|	Increases the value of a variable by 1	|	++x	     |
-		--	 |	Decrement		|	Decreases the value of a variable by 1	|	--x      |
+		++	 |	Increment		|	Increases the value of a variable by 1		|	++x	 |
+		--	 |	Decrement		|	Decreases the value of a variable by 1		|	--x      |
 	--------------------------------------------------------------------------------------
 	***********************************************************************************************
 	
 	2. Java Assignment Operators
 	---------------------------
-	OPERATOR |  EXAMPLE  |   SAME AS
+		OPERATOR   |   EXAMPLE  |   SAME AS
 		=		x = 5		x = 5	
 		+=		x += 3		x = x + 3	
 		-=		x -= 3		x = x - 3	
@@ -43,30 +43,30 @@ package learning;
 		
 	3. Java Comparison Operators
 	---------------------------
-	OPERATOR |       NAME                  |    EXAMPLE
-		==			Equal to					x == y	
-		!=			Not equal					x != y	
-		>			Greater than				x > y	
-		<			Less than					x < y	
+	      OPERATOR     |              NAME                   |      EXAMPLE
+		==			Equal to			x == y	
+		!=			Not equal			x != y	
+		>			Greater than			x > y	
+		<			Less than			x < y	
 		>=			Greater than or equal to	x >= y	
 		<=			Less than or equal to		x <= y
 		
 	************************************************************************************************
 	
 	4. Java Logical Operators
-	-------------------------------------------------------------------------------------------------------------
-	OPERATOR |    NAME       |        DESCRIPTION                                        |    EXAMPLE           |
-	-------------------------------------------------------------------------------------------------------------
-		&& 	 |	Logical and	 |	Returns true if both statements are true				 |	x < 5 &&  x < 10    |	
-		|| 	 |	Logical or	 |	Returns true if one of the statements is true			 |	x < 5 || x < 4	    |
-		!	 |	Logical not	 |	Reverse the result, returns false if the result is true	 |	!(x < 5 && x < 10)  |
-	-------------------------------------------------------------------------------------------------------------	
+	-----------------------------------------------------------------------------------------------------------------------------------------------------
+	OPERATOR 	  |    NAME      	 |        DESCRIPTION                                        			|    EXAMPLE           	    |
+	-----------------------------------------------------------------------------------------------------------------------------------------------------
+		&& 	 |	Logical and	 |	Returns true if both statements are true				|	x < 5 &&  x < 10    |	
+		|| 	 |	Logical or	 |	Returns true if one of the statements is true			 	|	x < 5 || x < 4	    |
+		!	 |	Logical not	 |	Reverse the result, returns false if the result is true	 		|	!(x < 5 && x < 10)  |
+	-----------------------------------------------------------------------------------------------------------------------------------------------------	
 	
 	***********************************************************************************************************
 	
 	5. Java Bitwise Operators
 	----------------------
-	OPERATOR | NAME
+		OPERATOR  |    NAME
 		&		AND 
 		|		OR 
 		~		NOT 
